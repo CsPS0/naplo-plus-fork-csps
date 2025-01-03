a6bf70ed841f6e767d3b56fee647d52d51418683
Marton Kiss <kima.public@gmail.com>
2025-01-03 17:04:56 +0100

    removed dynamic icon, fixed tools.py

--- lib/helpers/app_icon_helper.dart
+++ lib/helpers/app_icon_helper.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_dynamic_icon_plus/flutter_dynamic_icon_plus.dart';
+// import 'package:flutter_dynamic_icon_plus/flutter_dynamic_icon_plus.dart';
 
 class AppIconHelper {
   // static const _channel = MethodChannel('app_icon');
 
   static Future<void> setAppIcon(String iconName) async {
     try {
-      if (await FlutterDynamicIconPlus.supportsAlternateIcons) {
-        // await FlutterDynamicIconPlus.setAlternateIconName(iconName: "icon_new");
-        if (kDebugMode) {
-          print("successfully changed app icon");
-        }
-        return;
-      }
+      return;
+      // if (await FlutterDynamicIconPlus.supportsAlternateIcons) {
+      //   // await FlutterDynamicIconPlus.setAlternateIconName(iconName: "icon_new");
+      //   if (kDebugMode) {
+      //     print("successfully changed app icon");
+      //   }
+      //   return;
+      // }
     } on PlatformException {
       if (kDebugMode) {
         print("failed to change icon");
